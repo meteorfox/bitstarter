5a3e650be8511a3b28898eab81a75852bf71aa9d
Carlos L. Torres <ctorresk8guitar.upr@gmail.com>
2013-07-10 01:58:44 +0000

    Modified web.js to read contents from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync('index.html');
+var buf = new Buffer(contents);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
